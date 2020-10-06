88c9b1348da30cc0283200d2349ab133f0a02452
Timovzl <655426+Timovzl@users.noreply.github.com>
2020-10-06 17:49:16 +0200

    Added some TODOs.

--- EntityFramework.DbContextManagement/Observers/CommandInterceptor.cs
+++ EntityFramework.DbContextManagement/Observers/CommandInterceptor.cs
@@ -31,6 +31,7 @@ namespace Architect.EntityFramework.DbContextManagement.Observers
 
 		public override InterceptionResult<int> NonQueryExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<int> result)
 		{
+			// #TODO: Test if this is inadvertently hit on first query, when using SQL Server
 			this.WillExecuteNonQuery(false, command, default).AssumeSynchronous();
 
 			return base.NonQueryExecuting(command, eventData, result);
@@ -39,6 +40,7 @@ namespace Architect.EntityFramework.DbContextManagement.Observers
 		public override async ValueTask<InterceptionResult<int>> NonQueryExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<int> result,
 			CancellationToken cancellationToken = default)
 		{
+			// #TODO: Test if this is inadvertently hit on first query, when using SQL Server
 			await this.WillExecuteNonQuery(true, command, cancellationToken);
 
 			return await base.NonQueryExecutingAsync(command, eventData, result, cancellationToken);
